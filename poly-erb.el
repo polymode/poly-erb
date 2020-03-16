60abd9f01876e65dcfd904a1f4b60f3df6369ddd
Vitalie Spinu <spinuvit@gmail.com>
2020-03-16 13:59:52 +0100

    Version 0.2.1

--- poly-erb.el
+++ poly-erb.el
@@ -3,8 +3,8 @@
 ;; Author: Siavash Sajjadi and Vitalie Spinu
 ;; Maintainer: Vitalie Spinu
 ;; Copyright (C) 2018
-;; Version: 0.2
-;; Package-Requires: ((emacs "25") (polymode "0.2"))
+;; Version: 0.2.1
+;; Package-Requires: ((emacs "25") (polymode "0.2.1"))
 ;; URL: https://github.com/polymode/poly-erb
 ;; Keywords: emacs
 ;;
